93f334b528e56f9f04e2701321bac0c48995cf33
Vladimir Arhipov <xenialmoth@gmail.com>
2024-08-02 01:19:19 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -11,13 +11,21 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'Bot {bot.user} is ready.')
+    await clear_global_commands()
+    await clear_guild_commands()
 
-async def clear_slash_commands():
+async def clear_global_commands():
+    commands = await bot.http.get_global_application_commands(bot.user.id)
+    for command in commands:
+        await bot.http.delete_global_application_command(bot.user.id, command['id'])
+    print('Cleared all global commands.')
+
+async def clear_guild_commands():
     for guild in bot.guilds:
         commands = await bot.http.get_guild_application_commands(bot.user.id, guild.id)
         for command in commands:
             await bot.http.delete_guild_application_command(bot.user.id, guild.id, command['id'])
-        print(f'Cleared all slash commands in guild: {guild.name}')
+        print(f'Cleared all commands in guild: {guild.name}')
 
 @bot.slash_command(name="hello", description="Say hello")
 async def hello(ctx: discord.ApplicationContext):
@@ -25,7 +33,8 @@ async def hello(ctx: discord.ApplicationContext):
 
 @bot.event
 async def on_guild_join(guild):
-    await clear_slash_commands()
+    await clear_guild_commands()
 
-bot.loop.create_task(clear_slash_commands())
+bot.loop.create_task(clear_global_commands())
+bot.loop.create_task(clear_guild_commands())
 bot.run(os.getenv('DISCORD_TOKEN'))
