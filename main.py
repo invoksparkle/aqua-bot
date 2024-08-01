41ab33aa58dfd86fa175a7789205898ac3fe234c
Vladimir Arhipov <xenialmoth@gmail.com>
2024-08-02 01:17:38 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import discord
+from discord import Intents
 from discord.ext import commands
 
-intents = discord.Intents.default()
+intents = Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -11,8 +12,20 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Bot {bot.user} is ready.')
 
-@bot.slash_command(name="hello", description="Сказать привет")
-async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond("Привет!")
+async def clear_slash_commands():
+    for guild in bot.guilds:
+        commands = await bot.http.get_guild_application_commands(bot.user.id, guild.id)
+        for command in commands:
+            await bot.http.delete_guild_application_command(bot.user.id, guild.id, command['id'])
+        print(f'Cleared all slash commands in guild: {guild.name}')
 
+@bot.slash_command(name="hello", description="Say hello")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond('Hello!')
+
+@bot.event
+async def on_guild_join(guild):
+    await clear_slash_commands()
+
+bot.loop.create_task(clear_slash_commands())
 bot.run(os.getenv('DISCORD_TOKEN'))
