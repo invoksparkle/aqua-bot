9f968bf1827d70f0217ff33fe402e95aa091791f
Vladimir Arhipov <xenialmoth@gmail.com>
2024-08-03 00:34:37 +0300

    Добавление функционала прослушивания YouTube

--- dockerfile
+++ dockerfile
@@ -1,18 +1,18 @@
-# Используем официальный образ Python
+# Используем официальный образ Python 3.12
 FROM python:3.12
 
 # Устанавливаем рабочую директорию в контейнере
 WORKDIR /app
 
+# Устанавливаем FFmpeg
+RUN apt-get update && apt-get install -y ffmpeg
+
 # Копируем файл зависимостей и устанавливаем их
 COPY requirements.txt requirements.txt
-
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Копируем остальные файлы проекта в рабочую директорию
-
 COPY . .
 
 # Указываем команду для запуска бота
-
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import logging
 import discord
 from discord.ext import commands
+import youtube_dl
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -18,6 +19,21 @@ if GUILD_ID is None:
     raise ValueError("Переменная окружения GUILD_ID не установлена")
 GUILD_ID = int(GUILD_ID)
 
+# Настройка youtube_dl
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+ffmpeg_options = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn',
+}
+
 @bot.event
 async def on_ready():
     logger.info(f'Бот {bot.user} готов.')
@@ -27,5 +43,34 @@ async def on_ready():
 async def hello(ctx: discord.ApplicationContext):
     """Поздороваться с ботом"""
     await ctx.respond(f"Привет, {ctx.author}!")
+
+@bot.slash_command(guild_ids=[GUILD_ID])
+async def play(ctx: discord.ApplicationContext, url: str):
+    """Воспроизвести аудио из видео на YouTube"""
+    await ctx.defer()
+    voice_channel = ctx.author.voice.channel
+    if not voice_channel:
+        await ctx.respond("Вы должны быть в голосовом канале, чтобы использовать эту команду.")
+        return
+    
+    vc = await voice_channel.connect()
+    
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        url2 = info['formats'][0]['url']
+        source = await discord.FFmpegOpusAudio.from_probe(url2, **ffmpeg_options)
+        vc.play(source)
+    
+    await ctx.respond(f"Воспроизведение: {url}")
+
+@bot.slash_command(guild_ids=[GUILD_ID])
+async def stop(ctx: discord.ApplicationContext):
+    """Остановить воспроизведение и отключиться от голосового канала"""
+    vc = ctx.voice_client
+    if vc and vc.is_playing():
+        vc.stop()
+    if vc:
+        await vc.disconnect()
+    await ctx.respond("Остановлено и отключено от голосового канала.")
 
 bot.run(os.getenv('DISCORD_TOKEN'))
