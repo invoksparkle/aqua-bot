82e80aada6c973566d7bfab4a826792e4887bd96
Vladimir Arhipov <xenialmoth@gmail.com>
2024-08-03 00:04:20 +0300

    Setup bot with py-cord and guild command

--- main.py
+++ main.py
@@ -1,38 +1,23 @@
 import os
 import discord
-from discord import Intents
 from discord.ext import commands
 
-intents = Intents.default()
-intents.message_content = True
+intents = discord.Intents.default()
+intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = discord.Bot(intents=intents)
+
+# Получение ID гильдии из переменной окружения
+GUILD_ID = int(os.getenv('GUILD_ID'))
 
 @bot.event
 async def on_ready():
     print(f'Bot {bot.user} is ready.')
-    await clear_global_commands()
-    await clear_guild_commands()
 
-async def clear_global_commands():
-    global_commands = await bot.tree.fetch_global_commands()
-    for command in global_commands:
-        await bot.tree.delete_global_command(command.id)
-    print('Cleared all global commands.')
-
-async def clear_guild_commands():
-    for guild in bot.guilds:
-        guild_commands = await bot.tree.fetch_guild_commands(guild.id)
-        for command in guild_commands:
-            await bot.tree.delete_guild_command(guild.id, command.id)
-        print(f'Cleared all commands in guild: {guild.name}')
-
-@bot.tree.command(name="hello", description="Say hello")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message('Hello!')
-
-@bot.event
-async def on_guild_join(guild):
-    await clear_guild_commands()
+# Гильдейская команда
+@bot.slash_command(guild_ids=[GUILD_ID])
+async def hello(ctx: discord.ApplicationContext):
+    """Say hello to the bot"""
+    await ctx.respond(f"Hello {ctx.author}!")
 
 bot.run(os.getenv('DISCORD_TOKEN'))
