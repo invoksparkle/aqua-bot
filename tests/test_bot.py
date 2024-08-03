9de06f0f2933bd9ef59bc60b5513216762388a8f
Vladimir Arhipov <xenialmoth@gmail.com>
2024-08-03 11:50:08 +0300

    Добавление заглушки

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,6 +1,11 @@
+import os
 import unittest
+from unittest.mock import patch
 from discord.ext import commands
 from bot.general import GeneralCommands
+
+# Устанавливаем заглушку для GUILD_ID
+os.environ['GUILD_ID'] = '123456789'
 
 class TestBot(unittest.TestCase):
     def setUp(self):
